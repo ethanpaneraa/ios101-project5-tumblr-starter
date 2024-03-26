ebe3205c2d183318b524a2a86722768da517469b
Ethan Pineda <joshuapineda66@gmail.com>
2024-03-26 00:13:20 -0500

    done

--- ios101-project5-tumblr/ViewController.swift
+++ ios101-project5-tumblr/ViewController.swift
@@ -6,17 +6,34 @@
 import UIKit
 import Nuke
 
-class ViewController: UIViewController {
-
-
+class ViewController: UIViewController, UITableViewDataSource {
+        
+    private var posts: [Post] = []
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.dataSource = self
         
         fetchPosts()
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Post Cell", for: indexPath) as! PostCell
+        
+
+        let post = posts[indexPath.row]
+        if let photo = post.photos.first {
+            let url = photo.originalSize.url
+            Nuke.loadImage(with: url, into: cell.postImageView)
+        }
+        cell.label.text = post.summary
+        return cell
+    }
 
     func fetchPosts() {
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork/posts/photo?api_key=1zT8CiXGXFcQDyMFG7RtcfGLwTdDjFUJnZzKJaWTmgyK4lKGYk")!
@@ -38,18 +55,12 @@ class ViewController: UIViewController {
 
             do {
                 let blog = try JSONDecoder().decode(Blog.self, from: data)
-
-                DispatchQueue.main.async { [weak self] in
-
-                    let posts = blog.response.posts
-
-
-                    print("✅ We got \(posts.count) posts!")
-                    for post in posts {
-                        print("🍏 Summary: \(post.summary)")
-                    }
+                DispatchQueue.main.async {
+                    self.posts = blog.response.posts
+                    self.tableView.reloadData()
+//                    self.tableView.refreshControl?.endRefreshing()
+                    print("✅ We got \(self.posts.count) posts!")
                 }
-
             } catch {
                 print("❌ Error decoding JSON: \(error.localizedDescription)")
             }
